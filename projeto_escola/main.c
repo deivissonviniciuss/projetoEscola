ed9412fa301603961455d8184f7c3a99d576176b
Deivisson <deivissonviniciuss@gmail.com>
2024-10-18 10:41:32 -0300

    Update and rename escola.c to main.c

--- projeto_escola/main.c
+++ projeto_escola/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "aluno.h"
+#include "professor.h"
 
 int menuGeral();
 
 int main() {
     Aluno listaAluno[TAM_ALUNO];
+    Professor listaProfessor[TAM_PROFESSOR];
     int opcao;
     int qtdAluno = 0;
+    int qtdProfessor = 0;
     int sair = FALSE;
 
     // Tela inicial com as opções e leitura
@@ -81,6 +84,62 @@ int main() {
         }
         case 2: {
             printf("Módulo Professor\n");
+            int sairProfessor = FALSE;
+            int opcaoProfessor;
+
+            while (sairProfessor == FALSE) {
+                opcaoProfessor = menuProfessor();
+
+                switch (opcaoProfessor) {
+                case 0: {
+                    sairProfessor = TRUE;
+                    break;
+                }
+                case 1: {
+                    int retorno = cadastrarProfessor(listaProfessor, qtdProfessor);
+                    if (retorno == LISTA_CHEIA)
+                        printf("Lista de professores cheia\n");
+                    else if (retorno == MATRICULA_INVALIDA)
+                        printf("Matrícula inválida\n");
+                    else if (retorno == MATRICULA_EXISTENTE)
+                        printf("Matrícula já existente\n");
+                    else if (retorno == CPF_EXISTENTE)
+                        printf("CPF já existente\n");
+                    else if (retorno == SEXO_INVALIDO)
+                        printf("Sexo inválido\n");
+                    else {
+                        printf("Professor cadastrado com sucesso\n");
+                        qtdProfessor++;
+                    }
+                    break;
+                }
+                case 2: {
+                    listarProfessor(listaProfessor, qtdProfessor);
+                    break;
+                }
+                case 3: {
+                    int retorno = atualizarProfessor(listaProfessor, qtdProfessor);
+                    if (retorno == ATUALIZACAO_PROFESSOR_SUCESSO)
+                        printf("Professor atualizado com sucesso\n");
+                    else if (retorno == MATRICULA_INEXISTENTE)
+                        printf("Matrícula inexistente\n");
+                    else if (retorno == MATRICULA_INVALIDA)
+                        printf("Matrícula inválida\n");
+                    break;
+                }
+                case 4: {
+                    int retorno = excluirProfessor(listaProfessor, qtdProfessor);
+                    if (retorno == EXCLUSAO_PROFESSOR_SUCESSO) {
+                        printf("Professor excluído com sucesso\n");
+                        qtdProfessor--;
+                    } else if (retorno == MATRICULA_INEXISTENTE)
+                        printf("Matrícula inexistente\n");
+                    else if (retorno == MATRICULA_INVALIDA)
+                        printf("Matrícula inválida\n");
+                    break;
+                }
+                }
+            }
             break;
         }
         default: {
